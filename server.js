143c9f4106584805c441fb7ddd19b7e54f1272d4
Shawn Brinkman <shawn.brinkman@datuhealth.com>
2014-03-09 12:14:30 -0600

    fixed server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 // set up ======================================================================
 // get all the tools we need
-require('newrelic');
+
 var yaml_config = require('node-yaml-config');
 var config = yaml_config.load(__dirname + '/config/config.yml');
 var express  = require('express');
