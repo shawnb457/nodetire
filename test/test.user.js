2de3f2925b7ee3d396d7ef3d4e1b8dfe2fef8349
Shawn Brinkman <shawn.brinkman@datuhealth.com>
2014-03-09 12:05:41 -0600

    fixed test

--- test/test.user.js
+++ test/test.user.js
@@ -1,45 +1,14 @@
-var should = require("should");
-var mongoose = require('mongoose');
-var User = require("../app/models/user.js");
-var db;
+var request = require('supertest')
+  , express = require('express');
 
-describe('User', function() {
+var app = require('../app.js');
 
-before(function(done) {
- db = mongoose.connect('mongodb://JakeBrink:MagsBrink@troup.mongohq.com:10089/app22718780');
-   done();
- });
-
- after(function(done) {
-   mongoose.connection.close()
-   done();
- });
-
- beforeEach(function(done) {
-  var user = new User({
-    local['email']: '12345',
-    local.password: 'testy'
-  });
-
-  user.save(function(error) {
-    if (error) console.log('error' + error.message);
-    else console.log('no error');
-    done();
-   });
- });
-
- it('find a user by username', function(done) {
-    User.findOne({ local.email: '12345' }, function(err, account) {
-      user.email.should.eql('12345');
-      
-      done();
-    });
- });
-
- afterEach(function(done) {
-    User.remove({}, function() {
-      done();
-    });
- });
-
-});
+describe('GET', function(){
+  it('respond with json', function(done){
+    request(app)
+    .get('/clouds.json')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200, done);
+  })
+})
