2ad3e75a919be43dc737ed414f8fa014df096c36
Shawn Brinkman <shawn.brinkman@datuhealth.com>
2014-03-31 16:42:42 -0600

    added source folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: 'public/',
                     src: ['**'],
-                    dest: 'app/'
+                    dest: '/'
                 },
 
                 {src: ['views/**'], dest: '/'},
