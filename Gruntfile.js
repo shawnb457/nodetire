e04d7b864986098172132c9066d2fe2fa9505b03
Shawn Brinkman <shawn.brinkman@datuhealth.com>
2014-04-01 07:59:48 -0600

    added all files to the tar file

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function(grunt) {
                 },
                 files: [{
                     expand: true,
-                    cwd: '/',
+                   
                     src: ['**'],
                     dest: '/'
                 },
